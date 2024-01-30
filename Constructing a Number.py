3579d787604ca21aeb0a36e2eaded5fe34723ade
CodeWithMe <102638928+DarkDEmperor@users.noreply.github.com>
2024-01-30 13:22:12 +0530

    Update Constructing a Number.py

--- Constructing a Number.py
+++ Constructing a Number.py
@@ -1,1 +1,13 @@
+'''
+Manipulating numbers is at the core of a programmer's job. To test how well you know their properties, you are asked to solve the following problem.
+You are given  non-negative integers a1, a2, ..., an. You want to know whether it's possible to construct a new integer using all the digits of these numbers such that it would be divisible by 3. 
+You can reorder the digits as you want. The resulting number can contain leading zeros.
+'''
 
+def canConstruct(a):
+    # Return "Yes" or "No" denoting whether you can construct the required number.
+
+    res, s = (''.join([str(i) for i in a])), 0
+    for i in res: s += int(i)
+    if s % 3 == 0: return 'Yes'
+    return 'No'
